8a200e8ef78d5bcd572d3c36e939bb8fa63dc11c
胡超 <ajayhu.s@gmail.com>
2016-04-19 23:58:04 +0800

    modify lab5

--- Assembly/Lab5/Fifth-1/Fifth-1.c
+++ Assembly/Lab5/Fifth-1/Fifth-1.c
@@ -3,86 +3,72 @@
 
 #define	N 	1000
 
-char BUF_NAME[N][11];
-int BUF_SCORE[N][5];
-
-int Input();
+int Input(char BUF_NAME[40],int BUF_SCORE[16]);
 int Judge(char IN_NAME[11]);
 
 int main(int argc,char * argv[])
 {
-	memset(BUF_NAME,0,N*11*sizeof(char));
-	memset(BUF_SCORE,0,N*5*sizeof(int));
-	strcpy(BUF_NAME[0],"zhangsan");
-	strcpy(BUF_NAME[1],"lisi");
-	strcpy(BUF_NAME[998],"wangwu");
-	strcpy(BUF_NAME[999],"xuxiaohua");
-	BUF_SCORE[0][0]=100;
-	BUF_SCORE[0][1]=85;
-	BUF_SCORE[0][2]=80;
-	BUF_SCORE[1][0]=80;
-	BUF_SCORE[1][1]=100;
-	BUF_SCORE[1][2]=70;
-	BUF_SCORE[998][0]=70;
-	BUF_SCORE[998][1]=60;
-	BUF_SCORE[998][2]=80;
-	BUF_SCORE[999][0]=40;
-	BUF_SCORE[999][1]=55;
-	BUF_SCORE[999][2]=61;
-	Input();
+	char BUF_NAME[]={
+		'z','h','a','n','g','s','a','n','0','0',
+		'l','i','s','i','0','0','0','0','0','0',
+		'w','a','n','g','w','u','0','0','0','0',
+		'x','u','x','i','a','o','h','u','a','0'
+	};
+	int BUF_SCORE[]={
+		100, 85, 80, 0,
+		80, 100,70, 0,
+		70, 60, 80, 0,
+		40, 55, 61, 0
+	};
+	Input(BUF_NAME,BUF_SCORE);
+	return 0;
 }
 
-int Input()
+int Input(char BUF_NAME[40],int BUF_SCORE[16])
 {
 	char IN_NAME[11];
-	int 	yes=0;
+	int 	yes=0,n=0;
 	printf("Please enter the student's name:\n");
 	gets(IN_NAME);
 	while(Judge(IN_NAME)!=0)
 	{
-		__asm
-		{
-			LOPA:	MOV	DX,-1		;已比较姓名个数
-				MOV	AX,0
+		__asm{	
+				MOV	EDX,-1		;已比较姓名个数
+				MOV	EAX,0
 			;匹配姓名是否存在
-			NEXT:	INC	DX
-				MOV	CL,0		;输入姓名已比较字符串长度
-				MOV	DI,AX
-				DEC	DI
-				MOV	SI,-1		;偏移量
-				CMP	DX,N 		;是否循环完毕
+			NEXT:
+				INC	n
+				INC	EDX
+				MOV	EDI,EAX
+				DEC	EDI
+				MOV	ESI,-1		;偏移量
+				CMP	EDX,N 		;是否循环完毕
 				JNE	CBUF		;跳转至比较学生姓名字符
-				LEA	DX,TIP 		;提示学生不存在
-				MOV	AH,9
-				INT	21H
 				MOV	yes,0
 				JNC	E
-			CBUF:	INC	SI
-				INC	CL
-				INC	DI
-				MOV	BL,BUF[DI]
-				CMP	BL,IN_NAME[SI+2];比较姓名字符是否相同
+			CBUF:	
+				INC	ESI
+				INC	EDI
+				MOV	ECX,BUF_NAME
+				MOV	BL,[ECX+EDI]
+				CMP	IN_NAME[ESI],BL       ;比较姓名字符是否相同
 				JNE	TNEXT		;跳转至下一个学生
-				CMP	CL,IN_NAME[1]	;输入姓名的字符是否比较完毕
+				CMP	IN_NAME[ESI+1],'\0'	;输入姓名的字符是否比较完毕
 				JNE	CBUF		;跳转至比较姓名字符是否相同
-				CMP	BUF[DI+1],0	;检查数据段中姓名字符是否检查完毕
-				JE	INIT
-			TNEXT:	ADD	AX,11
+				CMP	BUF_NAME[EDI+1],'0'	;检查数据段中姓名字符是否检查完毕
+				JZ	INIT
+			TNEXT:	ADD	AX,10
 				JMP	NEXT
 
-				INIT:	MOV	AX,DI
-				MOV	BL,IN_NAME[1]
-				SUB	AX,BX
-				MOV	POIN,OFFSET BUF
-				ADD	POIN,AX
-				ADD	POIN,11
+			INIT:
 				MOV	CX,N
+				MOV	DI,0
 			;计算平均成绩
-			AVG:	LEA	DI,BUF+10
-				MOV	BL,BUF[DI]
-				MOV	AL,BUF[DI+1]
+			AVG:
+				MOV	EBX,BUF_SCORE[EDI * TYPE int]
+				MOV	EAX,BUF_SCORE[EDI * TYPE int+1 * TYPE int]
 				LEA	EAX,[EAX+EBX*2]
-				MOV	BL,BUF[DI+2]
+				MOV	EBX,BUF_SCORE[EDI * TYPE int+2 * TYPE int]
 				LEA	ESI,[EBX+EAX*2]
 				MOV	EAX,92492493H
 				IMUL	ESI
@@ -90,23 +76,25 @@ int Input()
 				SAR 	EDX,2
 				MOV	EAX,EDX
 				SHR	EAX,1FH
-				ADD	EAX,EDX
-				MOV	BUF[DI+3],AL
-				ADD	DI,14
+				ADD	EDX,EAX
+				MOV	BUF_SCORE[EDI * TYPE int+3 * TYPE int],EDX
+				ADD	DI,4
 				LOOP	AVG
-				DEC	COUNT
-				JNZ	LOPA
-				MOV	AX,1
+				MOV	yes,1
+			E:
 		}
 		if(yes==1)
 		{
-			if(BUF_SCORE[]>=90) printf("A");
-			else if(BUF_SCORE[]>=80) printf("B");
-			else if(BUF_SCORE[]>=70) printf("C");
-			else if(BUF_SCORE[]>=60) printf("D");
+			if(BUF_SCORE[3+n*4] >= 90) printf("A");
+			else if(BUF_SCORE[3+n*4] >= 80) printf("B");
+			else if(BUF_SCORE[3+n*4] >= 70) printf("C");
+			else if(BUF_SCORE[3+n*4] >= 60) printf("D");
 			else printf("F");
 		}
 		else printf("This name not exit!\n");
+		n=0;
+		printf("Please enter the student's name:\n");
+		gets(IN_NAME);
 	}
 	return 0;
 }
@@ -121,4 +109,5 @@ int Judge(char IN_NAME[11])
 		}
 	}
 	else return 1;
+	return 0;
 }
