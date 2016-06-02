caaa2d3d45366bf8931f394653bac1eedac0f8dc
huchao <hnhuchao1@163.com>
2016-06-02 23:55:28 +0800

    创建数字逻辑lab3_3

---
+++ Digital-Logic/lab3_3/lab3_3.srcs/sources_1/new/lab3_3.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2016/06/02 23:54:22
+// Design Name: 
+// Module Name: lab3_3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lab3_3(
+    input INPUT,CLK,
+    output OUT
+    );
+    
+endmodule
