c0097b49871bdb94dae6c50d73e37322e1b46048
chaohu <ajayhu.s@gmail.com>
2017-07-11 17:19:12 +0800

    修正多线程读取同一变量（做向量编号）导致执行结果不正确的错误

--- ParallelProgramming/lab1/lab1.cpp
+++ ParallelProgramming/lab1/lab1.cpp
@@ -1,34 +1,34 @@
 #include <pthread.h>
+#include <stdio.h>
 #include <iostream>
 using namespace::std;
 
 int A[10] = {0,0,0,0,0,0,0,0,0,0};
 int B[10] = {0,1,2,3,4,5,6,7,8,9};
-int C[10] = {9,8,7,6,5,4,3,2,1,0};
+int C[10] = {0,1,2,3,4,5,6,7,8,9};
 
 void *_add(void * num) {
 	int i = *(int *)num;
-	cout<<i<<"hehe\n";
 	A[i] = B[i] + C[i];
+	printf("result:%d\n",A[i]);
 	return (void *)0;
 }
 
 int main() {
+	int num[10] = {0,1,2,3,4,5,6,7,8,9};
     pthread_t t_id[10];
     int i = 0;
 
     for(i = 0;i <= 9;i++) {
-        if(pthread_create(&t_id[i],NULL,&_add,&i) != 0) {
+        if(pthread_create(&t_id[i],NULL,_add,&num[i]) != 0) {
 			cout<<"线程创建出错";
 		}
-		cout<<i<<"pppp";
     }
 
     for(i = 0;i <= 9;i++) {
 		if(pthread_join(t_id[i],NULL) != 0) {
 			cout<<"线程出错";
 		}
-		cout<<A[i]<<"\n";
     }
 
     return 0;
