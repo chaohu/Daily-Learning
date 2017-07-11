bf99fcf49eaeaa5a70b2a9f2df87d07ab97cd908
chaohu <ajayhu.s@gmail.com>
2017-07-11 16:58:17 +0800

    并行编程实验——openmp实现lab2

---
+++ ParallelProgramming/lab2/lab2.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+	> File Name: lab2.cpp
+	> Author: huchao
+	> Mail: hnhuchao1@163.com 
+	> Created Time: 2017年07月11日 星期二 16时31分50秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+#include <omp.h>
+
+int test() {
+	for(int i = 0;i < 10000;i++) {
+	}
+	return 0;
+}
+
+int main() {
+	int A[10] = {0,0,0,0,0,0,0,0,0,0};
+	int B[10] = {0,1,2,3,4,5,6,7,8,9};
+	int C[10] = {0,1,2,3,4,5,6,7,8,9};
+	
+	#pragma omp parallel for
+	for(int i = 0;i <= 9;i++) {
+		test();
+		A[i] = B[i] + C[i];
+		cout<<A[i]<<"\n";
+	}
+
+	return 0;
+}
