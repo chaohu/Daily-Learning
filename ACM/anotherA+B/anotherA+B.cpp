ad443dfe44306855553015722c7bcdfbc73fd599
chaohu <ajayhu.s@gmail.com>
2017-08-16 15:34:25 +0800

    写一些oj，为上机做准备

---
+++ ACM/anotherA+B/anotherA+B.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: anotherA+B.cpp
+	> Author: huchao
+	> Mail: hnhuchao1@163.com 
+	> Created Time: 2017年08月16日 星期三 14时49分30秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+	int a = 0;
+	while(scanf("%d",&a)) {
+		printf("0 %d\n",a);
+	}
+	return 0;
+}
+
---
+++ ACM/seeyou/seeyou.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: seeyou.cpp
+	> Author: huchao
+	> Mail: hnhuchao1@163.com 
+	> Created Time: 2017年08月16日 星期三 15时13分51秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num = 0,ops = 0;
+	int i = 0,j = 0;
+	cin>>num;
+	for(i = 0;i < num;i++) {
+		cin>>ops;
+		for(j = 0;j < ops;j++) {
+		}
+	}
+	return 0;
+}
